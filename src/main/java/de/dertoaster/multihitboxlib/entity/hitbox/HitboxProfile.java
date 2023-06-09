c34b2ef4bc6ea4dbed08499050d22528cf9cd9e1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-06-09 11:59:52 +0200

    add option to not track

--- src/main/java/de/dertoaster/multihitboxlib/entity/hitbox/HitboxProfile.java
+++ src/main/java/de/dertoaster/multihitboxlib/entity/hitbox/HitboxProfile.java
@@ -7,6 +7,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 public record HitboxProfile(
 		AssetEnforcementConfig assetConfig,
+		boolean syncToModel,
 		List<String> synchedBones,
 		MainHitboxConfig mainHitboxConfig,
 		List<SubPartConfig> partConfigs
@@ -15,6 +16,7 @@ public record HitboxProfile(
 	public static final Codec<HitboxProfile> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
 				AssetEnforcementConfig.CODEC.fieldOf("synched-assets").forGetter(HitboxProfile::assetConfig),
+				Codec.BOOL.fieldOf("sync-with-model").forGetter(HitboxProfile::syncToModel),
 				Codec.STRING.listOf().fieldOf("synched-bones").forGetter(HitboxProfile::synchedBones),
 				MainHitboxConfig.CODEC.fieldOf("main-hitbox").forGetter(HitboxProfile::mainHitboxConfig),
 				SubPartConfig.CODEC.listOf().fieldOf("parts").forGetter(HitboxProfile::partConfigs)
