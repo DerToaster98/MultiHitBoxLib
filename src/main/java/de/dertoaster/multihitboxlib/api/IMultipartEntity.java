26c116bb295ed3b6c4aa46c207836148f2d74932
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-06-10 16:19:05 +0200

    add basic (non synched) alignment code

--- src/main/java/de/dertoaster/multihitboxlib/api/IMultipartEntity.java
+++ src/main/java/de/dertoaster/multihitboxlib/api/IMultipartEntity.java
@@ -1,13 +1,21 @@
 package de.dertoaster.multihitboxlib.api;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.function.BiConsumer;
 
 import javax.annotation.Nullable;
 
+import de.dertoaster.multihitboxlib.entity.MHLibPartEntity;
+import de.dertoaster.multihitboxlib.entity.hitbox.HitboxProfile;
+import de.dertoaster.multihitboxlib.entity.hitbox.SubPartConfig;
 import de.dertoaster.multihitboxlib.util.BoneInformation;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.entity.PartEntity;
 
 public interface IMultipartEntity<T extends Entity> {
@@ -22,5 +30,71 @@ public interface IMultipartEntity<T extends Entity> {
 	}
 	
 	public boolean syncWithModel();
+	
+	public default void createSubPartsFromProfile(final HitboxProfile profile, final T parentEntity, final BiConsumer<String, MHLibPartEntity<T>> storageFunction) {
+		int subPartNumber = 0;
+		for(SubPartConfig spc : profile.partConfigs()) {
+			MHLibPartEntity<T> part = this.createNewPartFrom(spc, parentEntity, subPartNumber);
+			subPartNumber++;
+			
+			storageFunction.accept(spc.name(), part);
+		}
+	}
+	
+	public default MHLibPartEntity<T> createNewPartFrom(SubPartConfig spc, final T parentEntity, final int subPartNumber) {
+		return new MHLibPartEntity<T>(parentEntity, spc);
+	}
+	
+	public Optional<MHLibPartEntity<T>> getPartByName(final String name);  
 
+	public default void alignSubParts(T entity, final Collection<MHLibPartEntity<T>> parts) {
+		final double curX = entity.getX();
+		final double curY = entity.getY();
+		final double curZ = entity.getZ();
+		
+		final float rotX = this.mhlibGetEntityRotationXForPartOffset();
+		final float rotY = this.mhlibGetEntityRotationYForPartOffset();
+		final float rotZ = this.mhlibGetEntityRotationZForPartOffset();
+		
+		final double entityScale = this.mhlibGetEntitySizeScale(entity);
+		
+		for(MHLibPartEntity<T> part : parts) {
+			Vec3 partOffset = part.getConfigPositionOffset();
+			partOffset = partOffset.xRot(rotX);
+			partOffset = partOffset.yRot(rotY);
+			partOffset = partOffset.zRot(rotZ);
+			
+			partOffset = partOffset.scale(entityScale);
+			
+			part.setPos(partOffset.add(curX, curY, curZ));
+		}
+	}
+	
+	public default void tickParts(T entity, final Collection<MHLibPartEntity<T>> parts) {
+		for(MHLibPartEntity<T> part : parts) {
+			part.tick();
+		}
+	}
+
+	public default double mhlibGetEntitySizeScale(T entity) {
+		if(entity instanceof AgeableMob am) {
+			if(am.isBaby()) {
+				return 0.5D;
+			}
+		}
+		return 1;
+	}
+
+	public default float mhlibGetEntityRotationXForPartOffset() {
+		return 0;
+	}
+	
+	public default float mhlibGetEntityRotationYForPartOffset() {
+		return 0;
+	}
+	
+	public default float mhlibGetEntityRotationZForPartOffset() {
+		return 0;
+	}
+	
 }
--- src/main/java/de/dertoaster/multihitboxlib/mixin/entity/MixinLivingEntity.java
+++ src/main/java/de/dertoaster/multihitboxlib/mixin/entity/MixinLivingEntity.java
@@ -3,16 +3,22 @@ package de.dertoaster.multihitboxlib.mixin.entity;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.BiConsumer;
 
 import javax.annotation.Nullable;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import de.dertoaster.multihitboxlib.api.IModifiableMultipartEntity;
 import de.dertoaster.multihitboxlib.api.IMultipartEntity;
+import de.dertoaster.multihitboxlib.entity.MHLibPartEntity;
 import de.dertoaster.multihitboxlib.entity.hitbox.HitboxProfile;
 import de.dertoaster.multihitboxlib.init.MHLibDatapackLoaders;
 import de.dertoaster.multihitboxlib.util.BoneInformation;
+import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -21,21 +27,94 @@ import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.entity.PartEntity;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity extends Entity implements IMultipartEntity<Entity> {
+public abstract class MixinLivingEntity extends Entity implements IMultipartEntity<LivingEntity> {
 
 	private static final EntityDataAccessor<Optional<UUID>> CURRENT_MASTER = SynchedEntityData.defineId(MixinLivingEntity.class, EntityDataSerializers.OPTIONAL_UUID);
 
-	private final Optional<HitboxProfile> HITBOX_PROFILE;
+	private Optional<HitboxProfile> HITBOX_PROFILE;
 
 	private EntityDimensions _mhlib_MainHitboxDimensions;
 
+	protected Map<String, MHLibPartEntity<LivingEntity>> partMap;
+	private PartEntity<?>[] partArray;
+
 	public MixinLivingEntity(EntityType<?> pEntityType, Level pLevel) {
 		super(pEntityType, pLevel);
-
-		this.HITBOX_PROFILE = MHLibDatapackLoaders.getHitboxProfile(pEntityType);
 	}
+	
+	@Inject(
+			method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/level/Level;)V",
+			at = @At("TAIL")
+			)
+	private void mixinConstructor(CallbackInfo ci) {
+		// Load base profile
+		this.HITBOX_PROFILE = MHLibDatapackLoaders.getHitboxProfile(this.getType());
+		
+		// Initialize map and array
+		int partCount = this.HITBOX_PROFILE.isPresent() ? this.HITBOX_PROFILE.get().partConfigs().size() : 0;
+		this.partMap = new Object2ObjectArrayMap<>(partCount);
+		this.partArray = new PartEntity<?>[partCount];
+		
+		if(this.HITBOX_PROFILE.isPresent()) {
+			// At last, create the parts themselves
+			final BiConsumer<String, MHLibPartEntity<LivingEntity>> storageFunction = (str, part) -> {
+				int id = 0;
+				while(partArray[id] != null) {
+					id++;
+				}
+				this.partArray[id] = part;
+				this.partMap.put(str, part);
+			};
+			this.createSubPartsFromProfile(this.HITBOX_PROFILE.get(), (LivingEntity)((Object)this), storageFunction);
+		}
+
+		if (this.isMultipartEntity() && this.getParts() != null) {
+			this.setId(Entity.ENTITY_COUNTER.getAndAdd(this.getParts().length + 1) + 1);
+		}
+	}
+
+	// After ticking all parts => call alignment code
+	// Bind to interface to potentially cancel auto ticking and alignment
+	@Inject(
+			method = "aiStep",
+			at = @At("TAIL")
+	)
+	private void mixinAiStep(CallbackInfo ci) {
+		if(!this.isMultipartEntity()) {
+			return;
+		}
+		
+		if (this.HITBOX_PROFILE.isPresent() && this.HITBOX_PROFILE.get().syncToModel()) {
+			// EValuate model data
+		} else {
+			this.alignSubParts((LivingEntity)(Object)this, this.partMap.values());
+		}
+	}
+	
+	@Inject(
+			method = "defineSynchedData",
+			at = @At("TAIL")
+	)
+	private void mixinDefineSynchedData(CallbackInfo ci) {
+		this.entityData.define(CURRENT_MASTER, Optional.empty());
+	}
+	
+	// In tick method => intercept and tick the subparts
+	@Inject(
+			method = "tick",
+			at = @At("TAIL")
+	)
+	private void mixinTick(CallbackInfo ci) {
+		if(!this.isMultipartEntity()) {
+			return;
+		}
+		
+		this.tickParts((LivingEntity)(Object)this, this.partMap.values());
+	}
+	
 
 	@Override
 	@Nullable
@@ -60,8 +139,22 @@ public abstract class MixinLivingEntity extends Entity implements IMultipartEnti
 			return;
 		}
 		// Process the bones...
+		for (Map.Entry<String, BoneInformation> entry : boneInformation.entrySet()) {
+			Optional<MHLibPartEntity<LivingEntity>> optPart = this.getPartByName(entry.getKey());
+			optPart.ifPresent(part -> {
+				
+			});
+		}
 	}
 	
+	@Override
+	public Optional<MHLibPartEntity<LivingEntity>> getPartByName(String name) {
+		if (this.partMap == null) {
+			return Optional.empty();
+		}
+		return Optional.ofNullable(this.partMap.getOrDefault(name, null));
+	}
+
 	@Override
 	public boolean syncWithModel() {
 		// Allow change of logic
@@ -73,10 +166,6 @@ public abstract class MixinLivingEntity extends Entity implements IMultipartEnti
 
 	// Before the constructor gets called => intercept the entityType and modify it's "size" argument to use the mainHitboxSize
 
-	// In tick method => intercept and tick the subparts
-	// After that => call alignment code
-	// Bind to interface to potentially cancel auto ticking and alignment
-
 	// Intercept defineSynchedData and add our master data
 
 	// Intercept object creation, then create the hitbox profile
@@ -86,7 +175,43 @@ public abstract class MixinLivingEntity extends Entity implements IMultipartEnti
 	// Call this after the object has been created => method in interface, needs to be cancellable
 
 	// Override setID to also set the id of the parts
+	@Override
+	public void setId(int pId) {
+		super.setId(pId);
+		
+		if (this instanceof IModifiableMultipartEntity<?>) {
+			return;
+		}
+		if (this.isMultipartEntity() && this.getParts() != null) {
+			for (int i = 0; i < this.getParts().length; i++) {
+				this.getParts()[i].setId(pId + i + 1);
+			}
+		}
+	}
+
+	@Override
+	public boolean isMultipartEntity() {
+		return super.isMultipartEntity() || !this.partMap.values().isEmpty();
+	}
+
+	@Override
+	@Nullable
+	public PartEntity<?>[] getParts() {
+		if (this instanceof IModifiableMultipartEntity<?>) {
+			return super.getParts();
+		}
+		return this.partArray;
+	}
 
 	// Also make sure to modify the result of isMultipartEntity to be correct
+	
+	@Override
+	public float mhlibGetEntityRotationYForPartOffset() {
+		if (this instanceof IModifiableMultipartEntity<?> imme) {
+			return imme.mhlibGetEntityRotationYForPartOffset();
+		}
+		
+		return this.getYRot();
+	}
 
 }
