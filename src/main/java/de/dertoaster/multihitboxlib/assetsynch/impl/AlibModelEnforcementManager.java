1e61059007e3035f622d54ffd77dd5fd494667ce
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-15 15:45:52 +0200

    glib and alib model loading

--- src/main/java/de/dertoaster/multihitboxlib/assetsynch/impl/AlibModelEnforcementManager.java
+++ src/main/java/de/dertoaster/multihitboxlib/assetsynch/impl/AlibModelEnforcementManager.java
@@ -3,19 +3,22 @@ package de.dertoaster.multihitboxlib.assetsynch.impl;
 import java.io.File;
 import java.util.Optional;
 
+import com.google.gson.JsonObject;
+
 import de.dertoaster.multihitboxlib.Constants;
+import de.dertoaster.multihitboxlib.MHLibMod;
 import de.dertoaster.multihitboxlib.assetsynch.AbstractAssetEnforcementManager;
+import mod.azure.azurelib.cache.AzureLibCache;
+import mod.azure.azurelib.cache.object.BakedGeoModel;
+import mod.azure.azurelib.loading.json.FormatVersion;
+import mod.azure.azurelib.loading.json.raw.Model;
+import mod.azure.azurelib.loading.object.BakedModelFactory;
+import mod.azure.azurelib.loading.object.GeometryTree;
+import mod.azure.azurelib.util.JsonUtil;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 
 public class AlibModelEnforcementManager extends AbstractAssetEnforcementManager {
-
-	/*@Override
-	protected void registerAsset(ResourceLocation id, BakedGeoModel asset) {
-		if (AzureLibCache.getBakedModels().containsKey(id))
-			MHLibMod.LOGGER.debug("Overriding geckolib model with id <" + id.toString() + ">");
-
-		AzureLibCache.getBakedModels().put(id, asset);
-	}*/
 
 	@Override
 	protected Optional<byte[]> encodeData(ResourceLocation id) {
@@ -28,7 +31,31 @@ public class AlibModelEnforcementManager extends AbstractAssetEnforcementManager
 
 	@Override
 	protected boolean receiveAndLoad(ResourceLocation id, byte[] data) {
-		return true;
+		if (AzureLibCache.getBakedModels().containsKey(id))
+			MHLibMod.LOGGER.debug("Overriding azurelib model with id <" + id.toString() + ">");
+		
+		Optional<Model> optModel = this.createBakedModel(data);
+		
+		optModel.ifPresent(rawModel -> {
+			if (rawModel.formatVersion() != FormatVersion.V_1_12_0) {
+				MHLibMod.LOGGER.warn("Unsupported geometry json version. Supported versions: 1.12.0, id: {}", id);
+			} else {
+				BakedGeoModel baked = BakedModelFactory.getForNamespace(id.getNamespace()).constructGeoModel(GeometryTree.fromModel(rawModel));
+				if (baked != null) {
+					AzureLibCache.getBakedModels().put(id, baked);
+				}
+			}
+		});
+		return optModel.isPresent() && optModel.get().formatVersion() == FormatVersion.V_1_12_0;
+	}
+
+	private Optional<Model> createBakedModel(byte[] data) {
+		String stringData = new String(data);
+		JsonObject jo = GsonHelper.fromJson(JsonUtil.GEO_GSON, stringData, JsonObject.class);
+		if (jo != null) {
+			return Optional.of(JsonUtil.GEO_GSON.fromJson(jo, Model.class));
+		}
+		return Optional.empty();
 	}
 
 	@Override
--- src/main/java/de/dertoaster/multihitboxlib/assetsynch/impl/GlibModelEnforcementManager.java
+++ src/main/java/de/dertoaster/multihitboxlib/assetsynch/impl/GlibModelEnforcementManager.java
@@ -3,19 +3,22 @@ package de.dertoaster.multihitboxlib.assetsynch.impl;
 import java.io.File;
 import java.util.Optional;
 
+import com.google.gson.JsonObject;
+
 import de.dertoaster.multihitboxlib.Constants;
+import de.dertoaster.multihitboxlib.MHLibMod;
 import de.dertoaster.multihitboxlib.assetsynch.AbstractAssetEnforcementManager;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import software.bernie.geckolib.cache.GeckoLibCache;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.loading.json.FormatVersion;
+import software.bernie.geckolib.loading.json.raw.Model;
+import software.bernie.geckolib.loading.object.BakedModelFactory;
+import software.bernie.geckolib.loading.object.GeometryTree;
+import software.bernie.geckolib.util.JsonUtil;
 
 public class GlibModelEnforcementManager extends AbstractAssetEnforcementManager {
-
-	/*@Override
-	protected void registerAsset(ResourceLocation id, BakedGeoModel asset) {
-		if (GeckoLibCache.getBakedModels().containsKey(id))
-			MHLibMod.LOGGER.debug("Overriding geckolib model with id <" + id.toString() + ">");
-
-		GeckoLibCache.getBakedModels().put(id, asset);
-	}*/
 
 	@Override
 	protected Optional<byte[]> encodeData(ResourceLocation id) {
@@ -28,7 +31,31 @@ public class GlibModelEnforcementManager extends AbstractAssetEnforcementManager
 
 	@Override
 	protected boolean receiveAndLoad(ResourceLocation id, byte[] data) {
-		return true;
+		if (GeckoLibCache.getBakedModels().containsKey(id))
+			MHLibMod.LOGGER.debug("Overriding geckolib model with id <" + id.toString() + ">");
+		
+		Optional<Model> optModel = this.createBakedModel(data);
+		
+		optModel.ifPresent(rawModel -> {
+			if (rawModel.formatVersion() != FormatVersion.V_1_12_0) {
+				MHLibMod.LOGGER.warn("Unsupported geometry json version. Supported versions: 1.12.0, id: {}", id);
+			} else {
+				BakedGeoModel baked = BakedModelFactory.getForNamespace(id.getNamespace()).constructGeoModel(GeometryTree.fromModel(rawModel));
+				if (baked != null) {
+					GeckoLibCache.getBakedModels().put(id, baked);
+				}
+			}
+		});
+		return optModel.isPresent() && optModel.get().formatVersion() == FormatVersion.V_1_12_0;
+	}
+
+	private Optional<Model> createBakedModel(byte[] data) {
+		String stringData = new String(data);
+		JsonObject jo = GsonHelper.fromJson(JsonUtil.GEO_GSON, stringData, JsonObject.class);
+		if (jo != null) {
+			return Optional.of(JsonUtil.GEO_GSON.fromJson(jo, Model.class));
+		}
+		return Optional.empty();
 	}
 
 	@Override
