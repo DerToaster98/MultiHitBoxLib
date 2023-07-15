68be8c673690df8e60ebe0074136c5b3ded29a4f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-15 13:38:40 +0200

    create config folders

--- src/main/java/de/dertoaster/multihitboxlib/MHLibMod.java
+++ src/main/java/de/dertoaster/multihitboxlib/MHLibMod.java
@@ -1,5 +1,8 @@
 package de.dertoaster.multihitboxlib;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -7,6 +10,7 @@ import com.mojang.logging.LogUtils;
 import de.dertoaster.multihitboxlib.assetsynch.AbstractAssetEnforcementManager;
 import de.dertoaster.multihitboxlib.init.MHLibDatapackLoaders;
 import de.dertoaster.multihitboxlib.init.MHLibPackets;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -29,18 +33,52 @@ public class MHLibMod {
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
 
+		// Now, initialize all our folders
+		initializeConfigDirectories();
+		
 		MHLibDatapackLoaders.init();
-		// Throws registration event and registers all asset enforcers
-		AbstractAssetEnforcementManager.init();
+	}
+
+	private static void initializeConfigDirectories() {
+		final File mainDir = Constants.MHLIB_CONFIG_DIR.get();
+		try {
+			checkAndCreateFolder(mainDir);
+			checkAndCreateFolder(Constants.MHLIB_ASSET_DIR);
+			checkAndCreateFolder(Constants.MHLIB_SYNC_DIR);
+		} catch (IOException e) {
+			e.printStackTrace();
+			// TODO: Log
+		}
+	}
+
+	public static void checkAndCreateFolder(File directory) throws IOException {
+		if (!directory.exists()) {
+			if (!directory.mkdirs()) {
+				throw new IOException("Unable to create directory <" + directory.getAbsolutePath() + "!");
+			}
+		} else if (!directory.isDirectory()) {
+			if (directory.delete()) {
+				checkAndCreateFolder(directory);
+			} else {
+				throw new IOException("Directory <" + directory.getAbsolutePath() + "> is a file and could not be deleted!");
+			}
+		}
 	}
 
 	private void commonSetup(final FMLCommonSetupEvent event) {
 		MHLibPackets.init();
+		
+		// Throws registration event and registers all asset enforcers
+		AbstractAssetEnforcementManager.init();
 	}
 
 	// You can use SubscribeEvent and let the Event Bus discover methods to call
 	@SubscribeEvent
 	public void onServerStarting(ServerStartingEvent event) {
+	}
+
+	public static ResourceLocation prefixAssesEnforcementManager(String string) {
+		return new ResourceLocation(Constants.MODID, "asset_manager/" + string);
 	}
 
 }
