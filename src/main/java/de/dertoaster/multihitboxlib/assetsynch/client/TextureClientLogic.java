4e834dd78e7fdf2251f58db97fe7f0b404817189
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-15 18:38:20 +0200

    workaround

--- src/main/java/de/dertoaster/multihitboxlib/assetsynch/client/TextureClientLogic.java
+++ src/main/java/de/dertoaster/multihitboxlib/assetsynch/client/TextureClientLogic.java
@@ -1,20 +1,13 @@
 package de.dertoaster.multihitboxlib.assetsynch.client;
 
 import java.io.IOException;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 
 import com.mojang.blaze3d.platform.NativeImage;
-import com.mojang.blaze3d.platform.TextureUtil;
-import com.mojang.blaze3d.systems.RenderSystem;
 
 import de.dertoaster.multihitboxlib.assetsynch.impl.TextureEnforcementManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.client.renderer.texture.DynamicTexture;
-import net.minecraft.client.resources.metadata.texture.TextureMetadataSection;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
 
 public class TextureClientLogic {
 	
@@ -27,40 +20,47 @@ public class TextureClientLogic {
 			return false;
 		}
 		final Minecraft mc = Minecraft.getInstance();
-		AbstractTexture loadedInternally;
-
-		try {
-			loadedInternally = mc.submit(() -> mc.getTextureManager().getTexture(id)).get();
-		}
-		catch (InterruptedException | ExecutionException e) {
-			throw new IOException("Failed to load original texture: " + id, e);
-		}
-		if (loadedInternally instanceof DynamicTexture dt) {
-			// If it is dynamic => easy, just reupload
-			dt.getPixels().copyFrom(ni);
-			dt.upload();
-		} else {
-			// Otherwise it gets icky...
-			RenderSystem.assertOnRenderThreadOrInit();
-			final int texId = TextureUtil.generateTextureId();
-			
-			Optional<Resource> textureBaseResource = Minecraft.getInstance().getResourceManager().getResource(id);
-			
-			Optional<TextureMetadataSection> textureBaseMeta;
-			try {
-				textureBaseMeta = textureBaseResource.isPresent() ? textureBaseResource.get().metadata().getSection(TextureMetadataSection.SERIALIZER) : Optional.empty();
-			} catch (IOException e) {
-				textureBaseMeta = Optional.empty();
-				e.printStackTrace();
-			}
-			boolean blur = textureBaseMeta.isPresent() && textureBaseMeta.get().isBlur();
-			boolean clamp = textureBaseMeta.isPresent() && textureBaseMeta.get().isClamp();
-			
-			TextureUtil.prepareImage(texId, 0, ni.getWidth(), ni.getHeight());
-			ni.upload(0, 0, 0, 0, 0, ni.getWidth(), ni.getHeight(), blur, clamp, false, true);
-		}
 		
+		DynamicTexture dynTex = new DynamicTexture(ni);
+		
+		mc.getTextureManager().register(id, dynTex);
 		return true;
+		
+		//Causes the client to freeze cause it waits for the submit call
+//		AbstractTexture loadedInternally;
+//
+//		try {
+//			loadedInternally = mc.submit(() -> mc.getTextureManager().getTexture(id)).get();
+//		}
+//		catch (InterruptedException | ExecutionException e) {
+//			throw new IOException("Failed to load original texture: " + id, e);
+//		}
+//		if (loadedInternally instanceof DynamicTexture dt) {
+//			// If it is dynamic => easy, just reupload
+//			dt.getPixels().copyFrom(ni);
+//			dt.upload();
+//		} else {
+//			// Otherwise it gets icky...
+//			RenderSystem.assertOnRenderThreadOrInit();
+//			final int texId = TextureUtil.generateTextureId();
+//			
+//			Optional<Resource> textureBaseResource = Minecraft.getInstance().getResourceManager().getResource(id);
+//			
+//			Optional<TextureMetadataSection> textureBaseMeta;
+//			try {
+//				textureBaseMeta = textureBaseResource.isPresent() ? textureBaseResource.get().metadata().getSection(TextureMetadataSection.SERIALIZER) : Optional.empty();
+//			} catch (IOException e) {
+//				textureBaseMeta = Optional.empty();
+//				e.printStackTrace();
+//			}
+//			boolean blur = textureBaseMeta.isPresent() && textureBaseMeta.get().isBlur();
+//			boolean clamp = textureBaseMeta.isPresent() && textureBaseMeta.get().isClamp();
+//			
+//			TextureUtil.prepareImage(texId, 0, ni.getWidth(), ni.getHeight());
+//			ni.upload(0, 0, 0, 0, 0, ni.getWidth(), ni.getHeight(), blur, clamp, false, true);
+//		}
+//		
+//		return true;
 	}
 
 }
