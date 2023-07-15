5cf25997d88196f54f564bbb00826c30aea185df
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-15 13:39:14 +0200

    add synch entry object for asset synching

---
+++ src/main/java/de/dertoaster/multihitboxlib/assetsynch/data/SynchEntryData.java
@@ -0,0 +1,36 @@
+package de.dertoaster.multihitboxlib.assetsynch.data;
+
+import java.util.List;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.resources.ResourceLocation;
+
+public record SynchEntryData(
+		ResourceLocation manager,
+		ResourceLocation id,
+		List<Byte> payload
+		) {
+	
+	public static final Codec<SynchEntryData> CODEC = RecordCodecBuilder.create(instance -> {
+		return instance.group(
+				ResourceLocation.CODEC.fieldOf("manager").forGetter(SynchEntryData::manager),
+				ResourceLocation.CODEC.fieldOf("id").forGetter(SynchEntryData::id),
+				Codec.BYTE.listOf().fieldOf("payload").forGetter(SynchEntryData::payload)
+			).apply(instance, SynchEntryData::new);
+	});
+
+	public boolean validate() {
+		return this.manager != null && !this.manager.toString().isEmpty() && this.id != null && !this.id.toString().isEmpty() && this.payload != null && !this.payload.isEmpty();
+	}
+	
+	public byte[] getPayLoadArray() {
+		byte[] result = new byte[this.payload.size()];
+		for (int i = 0; i < this.payload.size(); i++) {
+			result[i] = this.payload.get(i);
+		}
+		return result;
+	}
+
+}
