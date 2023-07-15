c24f0dd94fd7bef0e5584777c675be0bfcd39ba8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-15 17:59:46 +0200

    check if current client is the server

--- src/main/java/de/dertoaster/multihitboxlib/util/ClientOnlyMethods.java
+++ src/main/java/de/dertoaster/multihitboxlib/util/ClientOnlyMethods.java
@@ -1,6 +1,7 @@
 package de.dertoaster.multihitboxlib.util;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
@@ -16,6 +17,13 @@ public class ClientOnlyMethods {
 	@OnlyIn(Dist.CLIENT)
 	public static Level getWorld() {
 		return Minecraft.getInstance().level;
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public static boolean isCurrentPlayerOwnerIfIntegratedServer() {
+		Player p = getClientPlayer();
+		IntegratedServer integratedServer = Minecraft.getInstance().getSingleplayerServer();
+		return integratedServer != null && integratedServer.isSingleplayerOwner(p.getGameProfile());
 	}
 
 }
