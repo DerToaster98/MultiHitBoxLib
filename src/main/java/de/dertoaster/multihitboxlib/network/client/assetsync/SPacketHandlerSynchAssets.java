4e005bbc44c75557d4420818d0ca8d7420921ecb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-15 18:36:26 +0200

    fuck DistExecutor, annoying to use and only checks physical side

--- src/main/java/de/dertoaster/multihitboxlib/network/client/assetsync/SPacketHandlerSynchAssets.java
+++ src/main/java/de/dertoaster/multihitboxlib/network/client/assetsync/SPacketHandlerSynchAssets.java
@@ -8,15 +8,13 @@ import de.dertoaster.multihitboxlib.assetsynch.data.SynchDataContainer;
 import de.dertoaster.multihitboxlib.network.server.assetsync.SPacketSynchAssets;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.network.NetworkEvent.Context;
 
 public class SPacketHandlerSynchAssets extends AbstractSPacketHandlerCodecWrappingPacket<SynchDataContainer, SPacketSynchAssets>{
 
 	@Override
 	protected void execHandlePacket(SPacketSynchAssets packet, Supplier<Context> context, Level world, Player sender) {
-		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> AssetEnforcement.handlePacketData(packet.getData()));
+		AssetEnforcement.handlePacketData(packet.getData());
 	}
 
 }
