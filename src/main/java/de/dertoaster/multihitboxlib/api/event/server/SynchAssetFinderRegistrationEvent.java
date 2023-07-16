a06a1d9f2e86f326f68732b9112d1fbed4c8ad0a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-16 11:22:39 +0200

    implement IModBusEvent, otherwise crash

--- src/main/java/de/dertoaster/multihitboxlib/api/event/server/SynchAssetFinderRegistrationEvent.java
+++ src/main/java/de/dertoaster/multihitboxlib/api/event/server/SynchAssetFinderRegistrationEvent.java
@@ -5,8 +5,9 @@ import java.util.Map;
 import de.dertoaster.multihitboxlib.api.event.AbstractRegistrationEvent;
 import de.dertoaster.multihitboxlib.assetsynch.assetfinders.AbstractAssetFinder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.event.IModBusEvent;
 
-public class SynchAssetFinderRegistrationEvent extends AbstractRegistrationEvent<ResourceLocation, AbstractAssetFinder> {
+public class SynchAssetFinderRegistrationEvent extends AbstractRegistrationEvent<ResourceLocation, AbstractAssetFinder> implements IModBusEvent {
 
 	public SynchAssetFinderRegistrationEvent(Map<ResourceLocation, AbstractAssetFinder> map) {
 		super(map);
