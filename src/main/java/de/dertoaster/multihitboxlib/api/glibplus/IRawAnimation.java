6df0bae953730682cd49fe5215c003d0326a3a1c
RaveTr <yuyuss210@gmail.com>
2023-11-06 20:02:43 +0200

    more gradle changes

--- src/main/java/de/dertoaster/multihitboxlib/api/glibplus/IRawAnimation.java
+++ src/main/java/de/dertoaster/multihitboxlib/api/glibplus/IRawAnimation.java
@@ -21,6 +21,12 @@ public interface IRawAnimation {
      * @return The current tick progress of this animation instance.
      */
     double getCurAnimProgress();
+    /**
+     * Gets the speed modifier (in ticks) for this animation instance, usually defaults to {@code 1}.
+     *
+     * @return The speed modifier (in ticks) for this animation instance.
+     */
+    double getAnimSpeedModifier();
 
     /**
      * Gets the animatable owner of this animation instance.
@@ -28,4 +34,22 @@ public interface IRawAnimation {
      * @return The animatable owner of this animation instance.
      */
     IExtendedGeoAnimatableEntity getAnimatableOwner();
+
+    /**
+     * Plays this animation instance (leniently if {@code forceAnim} is {@code false}). Leniently-played animations will
+     * wait for any animation(s) currently playing in the owner {@link WrappedAnimationController} of this animation instance
+     * to stop.
+     *
+     * @param forceAnim Whether this animation instance should take priority and override any currently playing animation(s) in the owner {@link WrappedAnimationController}.
+     */
+    void playAnimation(boolean forceAnim);
+
+    /**
+     * Stops this animation instance (leniently of {@code lenientStop} is {@code true}). Leniently-stopped animations will
+     * attempt to stop <i>after</i> this animation instance is done. Only applies to non-looping and non-paused animations,
+     * otherwise it will normally/forcefully stop this animation instance.
+     *
+     * @param lenientStop
+     */
+    void stopAnimation(boolean lenientStop);
 }
