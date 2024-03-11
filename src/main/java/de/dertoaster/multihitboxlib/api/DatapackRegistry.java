a082a5213b3b14a17a43dd24de31bff51ed3c29f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-03-11 19:40:48 +0100

    add byNameCodecs

--- src/main/java/de/dertoaster/multihitboxlib/api/DatapackRegistry.java
+++ src/main/java/de/dertoaster/multihitboxlib/api/DatapackRegistry.java
@@ -9,6 +9,7 @@ import javax.annotation.Nullable;
 
 import com.mojang.serialization.Codec;
 
+import de.dertoaster.multihitboxlib.util.LazyLoadField;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
@@ -17,18 +18,27 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.DataPackRegistryEvent.NewRegistry;
 
-public record DatapackRegistry<T>(
-		ResourceKey<Registry<T>> registryKey,
-		Codec<T> objectCodec,
-		Codec<Holder<T>> registryCodec
-	) {
+public class DatapackRegistry<T> {
+		
+	protected final ResourceKey<Registry<T>> registryKey;
+	protected final Codec<T> objectCodec;
+	protected final Codec<Holder<T>> registryCodec;
 	
+	protected final LazyLoadField<Codec<T>> lazyLoadByNameCodec = new LazyLoadField<>(this::createByNameCodec);
+		
 	public DatapackRegistry(final ResourceLocation id, final Codec<T> codec) {
 		this(ResourceKey.createRegistryKey(id), codec);
 	}
 	
 	public DatapackRegistry(final ResourceKey<Registry<T>> resourceKey, final Codec<T> codec) {
 		this(resourceKey, codec, RegistryFileCodec.create(resourceKey, codec));
+	}
+	
+	public DatapackRegistry(final ResourceKey<Registry<T>> resourceKey, final Codec<T> codec, final Codec<Holder<T>> registryCodec) {
+		super();
+		this.registryCodec = registryCodec;
+		this.objectCodec = codec;
+		this.registryKey = resourceKey;
 	}
 
 	public void registerSynchable(NewRegistry registryEvent) {
@@ -66,6 +76,30 @@ public record DatapackRegistry<T>(
 			return Collections.emptyList();
 		}
 		return optRegistry.get().stream().collect(Collectors.toList());
+	}
+	
+	public ResourceKey<Registry<T>> registryKey() {
+		return this.registryKey;
+	}
+	
+	public Codec<T> objectCodec() {
+		return this.objectCodec;
+	}
+	
+	public Codec<T> byNameCodec() {
+		return this.lazyLoadByNameCodec.get();
+	}
+	
+	public Codec<Holder<T>> registryCodec() {
+		return this.registryCodec;
+	}
+	
+	protected Codec<T> createByNameCodec() {
+		return createByNameCodec(this);
+	}
+	
+	protected static <V> Codec<V> createByNameCodec(DatapackRegistry<V> registry) {
+		return registry.registryCodec().xmap(Holder::get, Holder::direct);
 	}
 	
 }
