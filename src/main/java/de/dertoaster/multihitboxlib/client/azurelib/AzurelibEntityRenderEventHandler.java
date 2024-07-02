0fdb92458d9b8fd9b2061b80923339bddbcb1c9f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-07-01 21:44:42 -0400

    builds against fixed Events Azurelib version

--- src/main/java/de/dertoaster/multihitboxlib/client/azurelib/AzurelibEntityRenderEventHandler.java
+++ src/main/java/de/dertoaster/multihitboxlib/client/azurelib/AzurelibEntityRenderEventHandler.java
@@ -2,31 +2,22 @@ package de.dertoaster.multihitboxlib.client.azurelib;
 
 import de.dertoaster.multihitboxlib.client.EntityRenderEventHandlerCommonLogic;
 import de.dertoaster.multihitboxlib.client.IBoneInformationCollectorLayerCommonLogic;
-import mod.azure.azurelib.event.GeoRenderEntityEvent;
 import mod.azure.azurelib.event.GeoRenderEvent;
-import mod.azure.azurelib.event.GeoRenderReplacedEntityEvent;
-import mod.azure.azurelib.event.NeoForgeGeoRenderPhaseEvent;
 import mod.azure.azurelib.renderer.GeoEntityRenderer;
 import mod.azure.azurelib.renderer.layer.GeoRenderLayer;
 import net.minecraft.world.entity.Entity;
 
 public class AzurelibEntityRenderEventHandler extends EntityRenderEventHandlerCommonLogic {
 
-	public static void onPostRenderEntity(NeoForgeGeoRenderPhaseEvent.NeoForgeGeoRenderEvent event) {
-		if (event.getGeoRenderEvent() == (GeoRenderReplacedEntityEvent.Post) event.getGeoRenderEvent())
-			return;
-		GeoRenderEntityEvent.Pre azureevent = (GeoRenderEntityEvent.Pre) event.getGeoRenderEvent();
-		Entity animatable = azureevent.getEntity();
-		performCommonLogic(azureevent.getPoseStack(), azureevent.getRenderer(), azureevent.getBufferSource(), azureevent.getPartialTick(), azureevent.getPackedLight(), animatable);
-		performGlibLogic(azureevent.getRenderer(), animatable);
+	public static void onPostRenderEntity(GeoRenderEvent.Entity.Post event) {
+		Entity animatable = event.getEntity();
+		performCommonLogic(event.getPoseStack(), event.getRenderer(), event.getBufferSource(), event.getPartialTick(), event.getPackedLight(), animatable);
+		performGlibLogic(event.getRenderer(), animatable);
 	}
 
-	public static void onPostRenderReplacedEntity(NeoForgeGeoRenderPhaseEvent.NeoForgeGeoRenderEvent event) {
-		if (event.getGeoRenderEvent() == (GeoRenderEntityEvent.Post) event.getGeoRenderEvent())
-			return;
-		GeoRenderReplacedEntityEvent.Post azureevent = (GeoRenderReplacedEntityEvent.Post) event.getGeoRenderEvent();
-		Entity animatable = azureevent.getReplacedEntity();
-		performCommonLogic(azureevent.getPoseStack(), azureevent.getRenderer(), azureevent.getBufferSource(), azureevent.getPartialTick(), azureevent.getPackedLight(), animatable);
+	public static void onPostRenderReplacedEntity(GeoRenderEvent.ReplacedEntity.Post event) {
+		Entity animatable = event.getReplacedEntity();
+		performCommonLogic(event.getPoseStack(), event.getRenderer(), event.getBufferSource(), event.getPartialTick(), event.getPackedLight(), animatable);
 	}
 	
 	private static void performGlibLogic(GeoEntityRenderer<?> geoRenderer, Entity entitybeingRenderer) {
