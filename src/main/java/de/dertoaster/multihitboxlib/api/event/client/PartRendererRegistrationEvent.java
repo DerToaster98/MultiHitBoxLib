18ce759838c7f673d5e575df7ed704ab0061bfc7
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-07-02 14:01:13 -0400

    Fix missing imports

--- src/main/java/de/dertoaster/multihitboxlib/api/event/client/PartRendererRegistrationEvent.java
+++ src/main/java/de/dertoaster/multihitboxlib/api/event/client/PartRendererRegistrationEvent.java
@@ -1,9 +1,13 @@
 package de.dertoaster.multihitboxlib.api.event.client;
 
+import de.dertoaster.multihitboxlib.api.event.AbstractRegistrationEvent;
 import de.dertoaster.multihitboxlib.entity.MHLibPartEntity;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.neoforged.fml.event.IModBusEvent;
+
+import java.util.Map;
+import java.util.function.Function;
 
 public class PartRendererRegistrationEvent extends AbstractRegistrationEvent<Class<? extends MHLibPartEntity<?>>, Function<EntityRenderDispatcher, ? extends EntityRenderer<? extends MHLibPartEntity<?>>>> implements IModBusEvent {
 
