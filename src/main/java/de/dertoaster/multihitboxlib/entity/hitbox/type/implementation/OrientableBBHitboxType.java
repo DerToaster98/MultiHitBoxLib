e10b96aef696b5fa8d9e4e9a7fffed0d5dd79762
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-07-20 13:55:32 +0200

    add comments on how to check if point is on or withing shape

--- src/main/java/de/dertoaster/multihitboxlib/entity/hitbox/type/implementation/OrientableBBHitboxType.java
+++ src/main/java/de/dertoaster/multihitboxlib/entity/hitbox/type/implementation/OrientableBBHitboxType.java
@@ -1,5 +1,10 @@
 package de.dertoaster.multihitboxlib.entity.hitbox.type.implementation;
 
 public class OrientableBBHitboxType {
+	
+	// Same as normal cube
+	// Subtract center loc from rotation
+	// Rotate loc by rotation of cube
+	// Check if loc is inside the constraints of the cube by simple checking for coords
 
 }
