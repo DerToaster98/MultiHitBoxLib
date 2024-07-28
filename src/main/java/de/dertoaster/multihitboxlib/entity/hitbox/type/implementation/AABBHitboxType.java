33cdaef605d30e91437a3ca6260658b4e4995efc
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-07-28 19:08:24 +0200

    remove rotation field as this is a axis aligned hitbox!

--- src/main/java/de/dertoaster/multihitboxlib/entity/hitbox/type/implementation/AABBHitboxType.java
+++ src/main/java/de/dertoaster/multihitboxlib/entity/hitbox/type/implementation/AABBHitboxType.java
@@ -15,13 +15,11 @@ import net.minecraft.world.phys.Vec3;
 public class AABBHitboxType implements IHitboxType {
 	
 	protected final Vec2 baseSize;
-	protected final Vec3 baseRotation; //Only to be used for rendering stuff. Won't change the hitbox as that is still axis alinged
 	protected final Vec3 basePosition;
 	protected final Vec3 pivotOffset;
 	
-	public AABBHitboxType(Vec2 size, Vec3 rot, Vec3 pos, Vec3 pivotOffset) {
+	public AABBHitboxType(Vec2 size, Vec3 pos, Vec3 pivotOffset) {
 		this.baseSize = size;
-		this.baseRotation = rot;
 		this.basePosition = pos;
 		this.pivotOffset = pivotOffset;
 	}
@@ -29,7 +27,6 @@ public class AABBHitboxType implements IHitboxType {
 	public static final Codec<AABBHitboxType> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
 				UtilityCodecs.VEC2_CODEC.fieldOf("size").forGetter(obj -> obj.baseSize),
-				Vec3.CODEC.optionalFieldOf("rotation", Vec3.ZERO).forGetter(obj -> obj.baseRotation),
 				Vec3.CODEC.fieldOf("position").forGetter(obj -> obj.basePosition),
 				Vec3.CODEC.optionalFieldOf("pivot-offset", Vec3.ZERO).forGetter(obj -> obj.pivotOffset)
 			).apply(instance, AABBHitboxType::new);
