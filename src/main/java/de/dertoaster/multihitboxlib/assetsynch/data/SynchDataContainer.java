83dfab55ee17558c96ca2e8e44f248b2f112d700
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-08-13 20:09:15 +0200

    add todo note

--- src/main/java/de/dertoaster/multihitboxlib/assetsynch/data/SynchDataContainer.java
+++ src/main/java/de/dertoaster/multihitboxlib/assetsynch/data/SynchDataContainer.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+// TODO: Create a method that creates a list of packets that can be sent to the client => Split into multiple blocks of 1MiB of data
 public record SynchDataContainer(
 		List<SynchDataManagerData> payload
 		) {
