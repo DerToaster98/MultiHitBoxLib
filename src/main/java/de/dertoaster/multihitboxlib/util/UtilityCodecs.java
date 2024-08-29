d11f27e70e195c69e395ecdfaeebf09bd23d1934
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-08-29 17:04:05 +0200

    add UUID codec

--- src/main/java/de/dertoaster/multihitboxlib/util/UtilityCodecs.java
+++ src/main/java/de/dertoaster/multihitboxlib/util/UtilityCodecs.java
@@ -1,20 +1,34 @@
 package de.dertoaster.multihitboxlib.util;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.mojang.serialization.Codec;
 
+import com.mojang.serialization.DataResult;
 import net.minecraft.Util;
 import net.minecraft.world.phys.Vec2;
 
 public class UtilityCodecs {
 
-	public static final Codec<Vec2> VEC2_CODEC = Codec.FLOAT.listOf().comapFlatMap((instance) -> {
-	      return Util.fixedSize(instance, 2).map((p_231081_) -> {
-	         return new Vec2(p_231081_.get(0), p_231081_.get(1));
-	      });
-	   }, (instance) -> {
-	      return List.of(instance.x, instance.y);
-	   });
-	
+    public static final Codec<Vec2> VEC2_CODEC = Codec.FLOAT.listOf().comapFlatMap((instance) -> {
+        return Util.fixedSize(instance, 2).map((p_231081_) -> {
+            return new Vec2(p_231081_.get(0), p_231081_.get(1));
+        });
+    }, (instance) -> {
+        return List.of(instance.x, instance.y);
+    });
+
+    public static final Codec<UUID> UUID_STRING_CODEC = Codec.STRING.comapFlatMap(
+            s ->
+            {
+                try {
+                    return DataResult.success(UUID.fromString(s));
+                } catch (Exception e) // fromString throws if it can't convert the string to a UUID
+                {
+                    return DataResult.error(e::getMessage);
+                }
+            },
+            UUID::toString);
+
 }
