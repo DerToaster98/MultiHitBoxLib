c85cf99682d7b8c1354adaffa5156578c8d7d525
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-09-04 18:00:16 +0200

    correct some things...

--- src/main/java/de/dertoaster/multihitboxlib/network/server/assetsync/SPacketSynchAssets.java
+++ src/main/java/de/dertoaster/multihitboxlib/network/server/assetsync/SPacketSynchAssets.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.DataResult;
 
 import de.dertoaster.multihitboxlib.api.network.AbstractSPacketCodecWrappingPacket;
 import de.dertoaster.multihitboxlib.assetsynch.data.SynchDataContainer;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 
 // TODO: Make sure this is at most 1 MiB in size. If it is larger, we need to send multiple packets!
 public class SPacketSynchAssets extends AbstractSPacketCodecWrappingPacket<SynchDataContainer, SPacketSynchAssets> {
@@ -15,11 +16,6 @@ public class SPacketSynchAssets extends AbstractSPacketCodecWrappingPacket<Synch
 	
 	public SPacketSynchAssets(SynchDataContainer data) {
 		super(data);
-	}
-
-	@Override
-	public Class<SPacketSynchAssets> getPacketClass() {
-		return SPacketSynchAssets.class;
 	}
 
 	@Override
@@ -42,6 +38,12 @@ public class SPacketSynchAssets extends AbstractSPacketCodecWrappingPacket<Synch
 	@Override
 	protected SPacketSynchAssets createPacket(SynchDataContainer data) {
 		return new SPacketSynchAssets(data);
+	}
+
+	@Override
+	public Type<? extends CustomPacketPayload> type() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
