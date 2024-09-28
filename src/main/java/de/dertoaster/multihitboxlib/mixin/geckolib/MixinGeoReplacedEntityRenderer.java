c570385edd6ca2304d44126947480ff55131f2eb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-09-28 12:31:14 +0200

    we need to mixin here too ... also adjust priorities
    (cherry picked from commit 2c7a3f9b1dd004f2e09bde97d63ef28edb2f846f)

--- src/main/java/de/dertoaster/multihitboxlib/mixin/geckolib/MixinGeoReplacedEntityRenderer.java
+++ src/main/java/de/dertoaster/multihitboxlib/mixin/geckolib/MixinGeoReplacedEntityRenderer.java
@@ -1,16 +1,29 @@
 package de.dertoaster.multihitboxlib.mixin.geckolib;
 
+import de.dertoaster.multihitboxlib.api.glibplus.MHLibExtendedGeoLayer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import de.dertoaster.multihitboxlib.client.geckolib.renderlayer.GeckolibBoneInformationCollectorLayer;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.renderer.GeoRenderer;
 import software.bernie.geckolib.renderer.GeoReplacedEntityRenderer;
 
-@Mixin(GeoReplacedEntityRenderer.class)
-public abstract class MixinGeoReplacedEntityRenderer {
-	
+import java.util.function.Consumer;
+
+@Mixin(value = GeoReplacedEntityRenderer.class, priority = Integer.MAX_VALUE)
+public abstract class MixinGeoReplacedEntityRenderer<E extends Entity, T extends GeoAnimatable> extends EntityRenderer<E> implements GeoRenderer<T> {
+
+	protected MixinGeoReplacedEntityRenderer(EntityRendererProvider.Context pContext) {
+		super(pContext);
+	}
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Inject(
 			method = "<init>(Lnet/minecraft/client/renderer/entity/EntityRendererProvider$Context;Lsoftware/bernie/geckolib/model/GeoModel;Lsoftware/bernie/geckolib/animatable/GeoAnimatable;)V",
@@ -19,6 +32,26 @@ public abstract class MixinGeoReplacedEntityRenderer {
 	private void mixinConstructor(CallbackInfo ci) {
 		GeoReplacedEntityRenderer self = (GeoReplacedEntityRenderer)(Object)this;
 		self.addRenderLayer(new GeckolibBoneInformationCollectorLayer(self));
+	}
+
+	@Unique
+	private void _mhlib_callLayers(final Consumer<MHLibExtendedGeoLayer> runPerLayer) {
+		GeoRenderer self = (GeoRenderer) this;
+		for (Object layerGeo : self.getRenderLayers()) {
+			if (layerGeo instanceof MHLibExtendedGeoLayer mhlibExtension) {
+				runPerLayer.accept(mhlibExtension);
+			}
+		}
+	}
+
+	@Inject(method = "renderRecursively", at = @At("HEAD"), remap = false)
+	private void mixinRenderRecursivelyStart(CallbackInfo ci) {
+		this._mhlib_callLayers(MHLibExtendedGeoLayer::onRenderRecursivelyStart);
+	}
+
+	@Inject(method = "renderRecursively", at = @At("TAIL"), remap = false)
+	private void mixinRenderRecursivelyEnd(CallbackInfo ci) {
+		this._mhlib_callLayers(MHLibExtendedGeoLayer::onRenderRecursivelyEnd);
 	}
 	
 }
