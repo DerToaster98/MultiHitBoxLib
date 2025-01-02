d66aa2b3516831ba02d7c6e446508587371347d9
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-01-02 12:53:48 +0100

    update EntityRendererMixin (needed later for orientable boxes)

--- src/main/java/de/dertoaster/multihitboxlib/mixin/minecraft/client/MixinEntityRenderDispatcher.java
+++ src/main/java/de/dertoaster/multihitboxlib/mixin/minecraft/client/MixinEntityRenderDispatcher.java
@@ -1,19 +1,18 @@
 package de.dertoaster.multihitboxlib.mixin.minecraft.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import de.dertoaster.multihitboxlib.entity.IOrientableHitbox;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
+import net.neoforged.neoforge.entity.PartEntity;
 import org.joml.Quaternionf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-
-import de.dertoaster.multihitboxlib.entity.IOrientableHitbox;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
-import net.minecraft.world.entity.Entity;
-import net.neoforged.neoforge.entity.PartEntity;
 
 @Mixin(EntityRenderDispatcher.class)
 public abstract class MixinEntityRenderDispatcher {
@@ -23,27 +22,22 @@ public abstract class MixinEntityRenderDispatcher {
 			method = "renderHitbox",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/client/renderer/LevelRenderer;renderLineBox(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDDDDFFFFFFF)V",
-					ordinal = 2
+					target = "Lnet/minecraft/client/renderer/LevelRenderer;renderLineBox(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/phys/AABB;FFFF)V",
+					ordinal = 1
 			),
 			locals = LocalCapture.CAPTURE_FAILSOFT
 	)
-	private void mixinDrawHitbox(
-			PoseStack pPoseStack, 
-			VertexConsumer pBuffer, 
-			Entity pEntity, 
-			float pPartialTicks,
-			// Local variables
-			PartEntity<?> enderdragonpart,
-			// CallbackInfo
-			CallbackInfo ci
-	) {
+	private static void mixinDrawHitbox(
+            PoseStack poseStack, VertexConsumer buffer, Entity p_entity, float red, float green, float blue, float alpha, CallbackInfo ci, AABB aabb, double d0, double d1, double d2, PartEntity[] var14, int var15, int var16, PartEntity enderdragonpart, double d3, double d4, double d5
+            // CallbackInfo
+            // Local variables
+    ) {
 		if (enderdragonpart != null && enderdragonpart instanceof IOrientableHitbox ioh) {
 			// Translate to center
-			pPoseStack.translate(ioh.getCenterOffset().x, ioh.getCenterOffset().y, ioh.getCenterOffset().z);
+			poseStack.translate(ioh.getCenterOffset().x, ioh.getCenterOffset().y, ioh.getCenterOffset().z);
 			// Rotate!
-			Quaternionf quat = new Quaternionf().rotateXYZ(ioh.getRotationX(), ioh.getRotationY(), ioh.getRotationZ()); 
-			pPoseStack.mulPose(quat);
+			Quaternionf quat = new Quaternionf().rotateXYZ(ioh.getRotationX(), ioh.getRotationY(), ioh.getRotationZ());
+			poseStack.mulPose(quat);
 		}
 	}
 	
